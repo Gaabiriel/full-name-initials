3ef796a3d26bf7b7371ac515e09e1581fb056937
GABRIEL DE SOUZA SILVA <GABRIEL.SI@uninter.com>
2018-01-10 14:39:58 -0200

    deleted backup folder

--- backup/100118/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnInit } from '@angular/core';
-import { ViewCell } from 'ng2-smart-table';
-export declare class BadgeNameInitialsComponent implements ViewCell, OnInit {
-    rowData: any;
-    value: string;
-    initials: string;
-    ngOnInit(): void;
-    getNameInitials(fullName: string): string;
-}
--- backup/100118/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var pluralize = require("pluralize");
-function getPlural() {
-    console.log(pluralize.plural("peito"));
-}
-exports.getPlural = getPlural;
